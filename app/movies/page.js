c12516958b0d5b92c56421ae94ee03c2415422d5
Dahmane Abdelmalek <dahmane.abdelmalek13@gmail.com>
2023-01-01 13:41:17 +0100

    make movie page get random data

--- app/movies/page.js
+++ app/movies/page.js
@@ -1,8 +1,15 @@
 import React from "react";
 import MoviesContainer from "../(components)/MoviesContainer";
+function randomIntFromInterval(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 const getMovies = async () => {
   const res = await fetch(
-    "https://api.themoviedb.org/3/trending/all/day?api_key=15d7a80a7c308b752bb115e6b0ff1757",
+    `https://api.themoviedb.org/3/trending/movie/day?api_key=15d7a80a7c308b752bb115e6b0ff1757&include_adult=false&page=${randomIntFromInterval(
+      1,
+      20
+    )}`,
     { cache: "no-store" }
   );
 
@@ -12,6 +19,7 @@ const getMovies = async () => {
 };
 async function Contact() {
   const data = await getMovies();
+  console.log(data);
   return (
     <div>
       <h1 className="w-3/4 left-0 right-0 my-0 mx-auto pl-10 mt-10 text-2xl uppercase">
